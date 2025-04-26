4529623c6303b69ec0298bce0adece00b1021409
Charindu Liyanage <charinduliyanage17@gmail.com>
2025-04-26 16:02:02 +0530

    web config

---
+++ SkillShare-backend/SkillShare-backend/src/main/java/log/spring/skillsharebackend/filter/WebConfig.java
@@ -0,0 +1,26 @@
+package log.spring.skillsharebackend.filter;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173","https://5bcf9f7e-e9bd-47e3-b724-3198bb940552-render.magicpatterns.app/")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+
+}
+
