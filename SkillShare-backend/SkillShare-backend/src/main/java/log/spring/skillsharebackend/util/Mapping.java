e22d6fbea96096d68a8925cd645d7122e3618424
Charindu Liyanage <charinduliyanage17@gmail.com>
2025-04-26 16:02:16 +0530

    Mapping

---
+++ SkillShare-backend/SkillShare-backend/src/main/java/log/spring/skillsharebackend/util/Mapping.java
@@ -0,0 +1,19 @@
+package log.spring.skillsharebackend.util;
+
+import log.spring.skillsharebackend.dto.impl.UserDto;
+import log.spring.skillsharebackend.entity.impl.UserEntity;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Mapping {
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+
+    public UserEntity toUserEntity(UserDto userDto) {
+        return modelMapper.map(userDto, UserEntity.class);
+    }
+}
