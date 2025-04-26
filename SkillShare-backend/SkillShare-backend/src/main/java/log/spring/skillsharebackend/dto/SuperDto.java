8df41ca2532051cc40e3f208439701206a119187
Charindu Liyanage <charinduliyanage17@gmail.com>
2025-04-26 13:24:00 +0530

    status about entites

---
+++ SkillShare-backend/SkillShare-backend/src/main/java/log/spring/skillsharebackend/dto/SuperDto.java
@@ -0,0 +1,5 @@
+package log.spring.skillsharebackend.dto;
+
+import java.io.Serializable;
+
+public interface SuperDto extends Serializable { }
---
+++ SkillShare-backend/SkillShare-backend/src/main/java/log/spring/skillsharebackend/dto/status/Status.java
@@ -0,0 +1,10 @@
+package log.spring.skillsharebackend.dto.status;
+
+
+
+import log.spring.skillsharebackend.dto.SuperDto;
+
+import java.io.Serializable;
+
+public interface Status extends Serializable , SuperDto {
+}
